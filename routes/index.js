58b93331f186a7d459b2ac80edb2fafa77d27581
zdchupka <zdchupka@wpi.edu>
2014-12-14 16:55:09 -0500

    Added Terbyte header message

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/terbyte.html', function(req, res) {
+  res.render('terbyte', { title: 'TerByte', message: 'A Team Plasma Innovation' });
+});
+
 module.exports = router;
