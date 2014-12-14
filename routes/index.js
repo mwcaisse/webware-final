6a25b75f04a876b3a595e4977c4a80b689729745
Trevor Behlman <tsbehlman@wpi.edu>
2014-12-14 18:07:14 -0500

    Redirect root url to index.html, added scaffolding for GET and POST requests

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.redirect('/index.html');
 });
 
 /* GET home page. */
-router.get('/terbyte.html', function(req, res) {
-  res.render('terbyte', { title: 'TerByte', message: 'A Team Plasma Innovation' });
+router.get('/index.html', function(req, res) {
+    res.render('terbyte', { title: 'TerByte', message: 'A Team Plasma Innovation' });
+});
+
+/* retrieve bug listing */
+router.get('/bug/all', function() {
+    
+});
+
+/* retrieve bug by id */
+router.get('/bug/id/:id', function() {
+    
+});
+
+/* retrieve user listing */
+router.get('/user/all', function() {
+    
+});
+
+/* retrieve user by id */
+router.get('/user/id/:id', function() {
+    
+});
+
+/* create a new bug */
+router.post('/bug/create', function() {
+    
+});
+
+/* update a bug */
+router.post('/bug/update', function() {
+    
+});
+
+/* create a comment */
+router.post('/comment', function() {
+    
 });
 
 module.exports = router;
